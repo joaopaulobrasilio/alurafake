c305a0c619f3aa8f59dda1e137f9fadf3db382be
Joao Paulo <joaopaulobrasilio947@gmail.com>
2025-12-15 01:15:15 -0300

    Adjust actions methods tests class user

--- src/test/java/br/com/alura/AluraFake/user/UserRepositoryTest.java
+++ src/test/java/br/com/alura/AluraFake/user/UserRepositoryTest.java
@@ -16,14 +16,15 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+
     @Test
     void findByEmail__should_return_existis_user() {
-        User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT);
+        User caio = new User("Andre", "andre@alura.com.br", Role.STUDENT);
         userRepository.save(caio);
 
-        Optional<User> result = userRepository.findByEmail("caio@alura.com.br");
+        Optional<User> result = userRepository.findByEmail("andre@alura.com.br");
         assertThat(result).isPresent();
-        assertThat(result.get().getName()).isEqualTo("Caio");
+        assertThat(result.get().getName()).isEqualTo("Andre");
 
         result = userRepository.findByEmail("sergio@alura.com.br");
         assertThat(result).isEmpty();
@@ -31,10 +32,10 @@ class UserRepositoryTest {
 
     @Test
     void existsByEmail__should_return_true_when_user_existis() {
-        User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT);
+        User caio = new User("Joao", "joao@alura.com.br", Role.STUDENT);
         userRepository.save(caio);
 
-        assertThat(userRepository.existsByEmail("caio@alura.com.br")).isTrue();
+        assertThat(userRepository.existsByEmail("joao@alura.com.br")).isTrue();
         assertThat(userRepository.existsByEmail("sergio@alura.com.br")).isFalse();
     }
 
